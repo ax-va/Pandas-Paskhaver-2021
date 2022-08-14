25799165d11d501a7e8818a1ebb7f7a92ed6551c
Alex Vasiliev <vasiliev.informatik@t-online.de>
2022-08-14 18:49:58 +0200

    update

---
+++ examples/example-3-5-series--invoking-function-by-apply.py
@@ -0,0 +1,93 @@
+"""
+Invoke a function on every Series value with the apply method
+"""
+
+import pandas as pd
+
+google = pd.read_csv("../datasets/google_stocks.csv", parse_dates=["Date"], index_col="Date").squeeze("columns")
+
+func_list = [len, max, min]
+for func in func_list:
+    print(func(google))
+# 3824
+# 1287.58
+# 49.82
+
+# The function rounds a value above 0.5 up and any value below 0.5 down:
+print(round(99.2))  # 99
+print(round(99.49))  # 99
+print(round(99.5))  # 100
+
+# The two lines below are equivalent
+s1 = google.apply(func=round)
+s2 = google.apply(round)
+
+print(s1)
+# Date
+# 2004-08-19      50
+# 2004-08-20      54
+# 2004-08-23      54
+# 2004-08-24      52
+# 2004-08-25      53
+#               ...
+# 2019-10-21    1246
+# 2019-10-22    1243
+# 2019-10-23    1259
+# 2019-10-24    1261
+# 2019-10-25    1265
+# Name: Close, Length: 3824, dtype: int64
+
+print(s2)
+# Date
+# 2004-08-19      50
+# 2004-08-20      54
+# 2004-08-23      54
+# 2004-08-24      52
+# 2004-08-25      53
+#               ...
+# 2019-10-21    1246
+# 2019-10-22    1243
+# 2019-10-23    1259
+# 2019-10-24    1261
+# 2019-10-25    1265
+# Name: Close, Length: 3824, dtype: int64
+
+
+def get_single_or_multi(pokemon_type):
+    if "/" in pokemon_type:
+        return "Multi"
+    return "Single"
+
+
+pokemon = pd.read_csv("../datasets/pokemon.csv", index_col="Pokemon").squeeze("columns")
+print(pokemon.head())
+
+# Pokemon
+# Bulbasaur     Grass / Poison
+# Ivysaur       Grass / Poison
+# Venusaur      Grass / Poison
+# Charmander              Fire
+# Charmeleon              Fire
+# Name: Type, dtype: object
+
+print(pokemon.apply(get_single_or_multi))
+# Pokemon
+# Bulbasaur       Multi
+# Ivysaur         Multi
+# Venusaur        Multi
+# Charmander     Single
+# Charmeleon     Single
+#                 ...
+# Stakataka       Multi
+# Blacephalon     Multi
+# Zeraora        Single
+# Meltan         Single
+# Melmetal       Single
+# Name: Type, Length: 809, dtype: object
+
+print(pokemon.apply(get_single_or_multi).value_counts())
+# Multi     405
+# Single    404
+# Name: Type, dtype: int64
+
+
---
+++ examples/example-3-6-series--coding-challenge.py
@@ -0,0 +1,74 @@
+"""
+Coding challenge
+"""
+
+import pandas as pd
+
+import datetime as dt
+day = dt.datetime(2020, 12, 26)
+print(day.strftime("%A"))  # Saturday
+
+
+battles_dataframe = pd.read_csv("../datasets/revolutionary_war.csv").head()
+print(battles_dataframe)
+#                               Battle  Start Date          State
+# 0                       Powder Alarm    9/1/1774  Massachusetts
+# 1  Storming of Fort William and Mary  12/14/1774  New Hampshire
+# 2   Battles of Lexington and Concord   4/19/1775  Massachusetts
+# 3                    Siege of Boston   4/19/1775  Massachusetts
+# 4                 Gunpowder Incident   4/20/1775       Virginia
+
+
+days_of_war_series = pd.read_csv(
+    filepath_or_buffer="../datasets/revolutionary_war.csv",
+    parse_dates=["Start Date"],
+    usecols=["Start Date"]
+).squeeze("columns")
+
+print(days_of_war_series.head())
+# 0   1774-09-01
+# 1   1774-12-14
+# 2   1775-04-19
+# 3   1775-04-19
+# 4   1775-04-20
+# Name: Start Date, dtype: datetime64[ns]
+
+
+def get_day_of_week(date):
+    return date.strftime("%A")
+
+
+# print(days_of_war_series.apply(get_day_of_week))
+# # ValueError: NaTType does not support strftime
+
+print(days_of_war_series.dropna().apply(get_day_of_week))
+# 0       Thursday
+# 1      Wednesday
+# 2      Wednesday
+# 3      Wednesday
+# 4       Thursday
+#          ...
+# 227    Wednesday
+# 228       Friday
+# 229       Friday
+# 230       Friday
+# 231    Wednesday
+# Name: Start Date, Length: 228, dtype: object
+
+print(days_of_war_series.dropna().apply(get_day_of_week).value_counts())
+# Saturday     39
+# Friday       39
+# Wednesday    32
+# Thursday     31
+# Sunday       31
+# Tuesday      29
+# Monday       27
+# Name: Start Date, dtype: int64
+
+
+
+
+
+
+
+
