b9e4f1b4af5acd02bc1cd4286a7d6bda6c46ff3a
Alex Vasiliev <vasiliev.informatik@t-online.de>
2022-10-08 00:04:33 +0200

    update

---
+++ examples/example-10-1-dataset-ops--datasets-to-be-used.py
@@ -0,0 +1,51 @@
+"""
+-- Applied pandas
+---- Merging, Joining, and Concatenating
+------ Introducing the Data Sets
+"""
+
+import pandas as pd
+
+groups1 = pd.read_csv("../datasets/meetup/groups1.csv")
+print(groups1.head())
+#    group_id                           name  category_id  city_id
+# 0      6388         Alternative Health NYC           14    10001
+# 1      6510      Alternative Energy Meetup            4    10001
+# 2      8458              NYC Animal Rights           26    10001
+# 3      8940  The New York City Anime Group           29    10001
+# 4     10104             NYC Pit Bull Group           26    10001
+
+# category_id and city_id are foreign keys
+
+groups2 = pd.read_csv("../datasets/meetup/groups2.csv")
+print(groups2.head(1))
+#    group_id          name  category_id  city_id
+# 0  18879327  BachataMania            5    10001
+
+categories = pd.read_csv("../datasets/meetup/categories.csv")
+print(categories.head())
+#    category_id            category_name
+# 0            1           Arts & Culture
+# 1            3       Cars & Motorcycles
+# 2            4  Community & Environment
+# 3            5                  Dancing
+# 4            6     Education & Learning
+
+cities = pd.read_csv("../datasets/meetup/cities.csv")
+print(cities.head())
+#       id            city state    zip
+# 0   7093   West New York    NJ   7093
+# 1  10001        New York    NY  10001
+# 2  13417  New York Mills    NY  13417
+# 3  46312    East Chicago    IN  46312
+# 4  56567  New York Mills    MN  56567
+
+cities = pd.read_csv("../datasets/meetup/cities.csv", dtype={"zip": "string"})
+print(cities.head())
+#       id            city state    zip
+# 0   7093   West New York    NJ  07093
+# 1  10001        New York    NY  10001
+# 2  13417  New York Mills    NY  13417
+# 3  46312    East Chicago    IN  46312
+# 4  56567  New York Mills    MN  56567
+
---
+++ examples/example-10-2-dataset-ops--concatenating-datasets.py
@@ -0,0 +1,110 @@
+"""
+-- Applied pandas
+---- Merging, Joining, and Concatenating
+------ Concatenating data Sets
+"""
+
+import pandas as pd
+
+groups1 = pd.read_csv("../datasets/meetup/groups1.csv")
+groups2 = pd.read_csv("../datasets/meetup/groups2.csv")
+categories = pd.read_csv("../datasets/meetup/categories.csv")
+cities = pd.read_csv("../datasets/meetup/cities.csv", dtype={"zip": "string"})
+
+groups1.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 7999 entries, 0 to 7998
+# Data columns (total 4 columns):
+#  #   Column       Non-Null Count  Dtype
+# ---  ------       --------------  -----
+#  0   group_id     7999 non-null   int64
+#  1   name         7998 non-null   object
+#  2   category_id  7999 non-null   int64
+#  3   city_id      7999 non-null   int64
+# dtypes: int64(3), object(1)
+# memory usage: 250.1+ KB
+
+groups2.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 8331 entries, 0 to 8330
+# Data columns (total 4 columns):
+#  #   Column       Non-Null Count  Dtype
+# ---  ------       --------------  -----
+#  0   group_id     8331 non-null   int64
+#  1   name         8331 non-null   object
+#  2   category_id  8331 non-null   int64
+#  3   city_id      8331 non-null   int64
+# dtypes: int64(3), object(1)
+# memory usage: 260.5+ KB
+
+groups = pd.concat(objs=[groups1, groups2])
+print(groups)
+#       group_id  ... city_id
+# 0         6388  ...   10001
+# 1         6510  ...   10001
+# 2         8458  ...   10001
+# 3         8940  ...   10001
+# 4        10104  ...   10001
+# ...        ...  ...     ...
+# 8326  26377464  ...   94101
+# 8327  26377698  ...   94101
+# 8328  26378067  ...   60601
+# 8329  26378128  ...   10001
+# 8330  26378470  ...   60601
+#
+# [16330 rows x 4 columns]
+
+# The groups DataFrame contains index duplicates
+
+assert len(groups1) + len(groups2) == len(groups)
+
+groups = pd.concat(objs=[groups1, groups2], ignore_index=True)
+print(groups)
+#        group_id  ... city_id
+# 0          6388  ...   10001
+# 1          6510  ...   10001
+# 2          8458  ...   10001
+# 3          8940  ...   10001
+# 4         10104  ...   10001
+# ...         ...  ...     ...
+# 16325  26377464  ...   94101
+# 16326  26377698  ...   94101
+# 16327  26378067  ...   60601
+# 16328  26378128  ...   10001
+# 16329  26378470  ...   60601
+#
+# [16330 rows x 4 columns]
+
+# Concatenation with MultiIndex
+groups = pd.concat(objs=[groups1, groups2], keys=["G1", "G2"])
+print(groups)
+#          group_id  ... city_id
+# G1 0         6388  ...   10001
+#    1         6510  ...   10001
+#    2         8458  ...   10001
+#    3         8940  ...   10001
+#    4        10104  ...   10001
+# ...           ...  ...     ...
+# G2 8326  26377464  ...   94101
+#    8327  26377698  ...   94101
+#    8328  26378067  ...   60601
+#    8329  26378128  ...   10001
+#    8330  26378470  ...   60601
+#
+# [16330 rows x 4 columns]
+
+print(groups.loc["G1"])
+#       group_id  ... city_id
+# 0         6388  ...   10001
+# 1         6510  ...   10001
+# 2         8458  ...   10001
+# 3         8940  ...   10001
+# 4        10104  ...   10001
+# ...        ...  ...     ...
+# 7994  18875285  ...   10001
+# 7995  18876571  ...   10001
+# 7996  18876811  ...   60601
+# 7997  18877490  ...   10001
+# 7998  18878056  ...   10001
+#
+# [7999 rows x 4 columns]
